10f5ec1d3df55b44b459feff19b059fb99d8af92
Rajat Rai <75417243+rajatrai30@users.noreply.github.com>
2026-06-27 16:03:51 +0530

    argocd image updater and CI CD connection test#2

--- src/frontend/genproto.sh
+++ src/frontend/genproto.sh
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# test
+
 # [START gke_frontend_genproto]
 
 PATH=$PATH:$(go env GOPATH)/bin
